64cdca9b0057a158f8dafcc40512bba7be64fa24
lithesh <litheshrajagongada@gmail.com>
2025-01-23 16:10:38 +0530

    pagination change

--- src/middleware/loggers/logger.js
+++ src/middleware/loggers/logger.js
@@ -2,7 +2,7 @@ const {createLogger,format,transports}=require('winston');
 
 module.exports.logger=createLogger({
     level:'info',
-    format:format.json(),
+    format:format.combine(format.colorize(),format.json()),
     transports:[
         new transports.File({filename:'./src/middleware/loggers/regularLogs.log',level:'info'}),
         new transports.File({filename:'./src/middleware/loggers/errorLogs.log',level:'error'})
